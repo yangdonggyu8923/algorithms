3f78b918e7cdb29063603445a25580007bba4e30
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-03-18 19:07:11 +0900

    240318 baekjoon blackjack

---
+++ baekjoon/bronze/BlackJack2798.java
@@ -0,0 +1,30 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+// 김정인 버전의 블랙잭에서 각 카드에는 양의 정수가 쓰여 있다.
+// 그 다음, 딜러는 N장의 카드를 모두 숫자가 보이도록 바닥에 놓는다.
+// 그런 후에 딜러는 숫자 M을 크게 외친다.
+//
+// 이제 플레이어는 제한된 시간 안에 N장의 카드 중에서 3장의 카드를 골라야 한다.
+// 블랙잭 변형 게임이기 때문에,
+// 플레이어가 고른 카드의 합은 M을 넘지 않으면서 M과 최대한 가깝게 만들어야 한다.
+//
+// N장의 카드에 써져 있는 숫자가 주어졌을 때,
+// M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 구해 출력하시오.
+public class BlackJack2798 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int total = 0;
+        int[] arr = new int[n];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+            total += arr[i];
+        }
+        int rs = total % m; //35%21
+        total = total - rs;
+        System.out.println(total);
+    }
+}
