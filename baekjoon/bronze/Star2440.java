6c3c4bb172f9fb97920909e4890bbff03b5f71c5
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-04-03 18:43:20 +0900

    240403 star3

--- baekjoon/bronze/Star2440.java
+++ baekjoon/bronze/Star2440.java
@@ -2,12 +2,12 @@ package baekjoon.bronze;
 
 import java.util.Scanner;
 
-public class Star2438 {
+public class Star2440 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i+1; j++) {
+        for(int i=0; i<n; i++){
+            for(int j=n; j>i; j--){
                 System.out.print("*");
             }
             System.out.println();
