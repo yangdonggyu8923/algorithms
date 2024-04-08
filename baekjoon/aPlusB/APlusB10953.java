b006572845242158ba54b32790c3e93da5a3ad42
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-04-08 19:50:10 +0900

    240408 10953

---
+++ baekjoon/aPlusB/APlusB10953.java
@@ -0,0 +1,17 @@
+package baekjoon.aPlusB;
+
+import java.util.Scanner;
+
+public class APlusB10953 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = "";
+        for(int i=0; i<n; i++){
+            s = sc.next();
+            int a = Integer.parseInt(s.substring(0,1));
+            int b = Integer.parseInt(s.substring(2,3));
+            System.out.println(a+b);
+        }
+    }
+}
