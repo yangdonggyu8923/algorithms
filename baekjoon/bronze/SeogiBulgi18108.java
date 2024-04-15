3d0443793225c0842b48f05ab9e956f84d76b637
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-04-15 09:29:09 +0900

    240415 baek

---
+++ baekjoon/bronze/SeogiBulgi18108.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class SeogiBulgi18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        System.out.println(y-543);
+    }
+}
