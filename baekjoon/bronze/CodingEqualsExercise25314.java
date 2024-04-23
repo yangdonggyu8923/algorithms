c5492e0fbd610f4b05d031217dadc8b24de3e2db
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-04-23 19:26:24 +0900

    24

---
+++ baekjoon/bronze/CodingEqualsExercise25314.java
@@ -0,0 +1,16 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class CodingEqualsExercise25314 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt()/4;
+        for(int i=0; i<n; i++){
+            System.out.print("long ");
+        }
+        System.out.print("int");
+
+
+    }
+}
