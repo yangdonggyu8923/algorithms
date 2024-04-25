1f7f9dde24a62c8e4781b3b479d7dd1dba7a49b9
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-04-25 09:34:38 +0900

    240425

---
+++ baekjoon/notyet/NUFastAPlusB15552.java
@@ -0,0 +1,14 @@
+package baekjoon.notyet;
+
+import java.util.Scanner;
+
+public class NUFastAPlusB15552 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i=0; i<4; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
---
+++ baekjoon/notyet/String9086.java
@@ -0,0 +1,28 @@
+package baekjoon.notyet;
+
+import java.util.Scanner;
+
+public class String9086 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0; i<t; i++){
+            String a = sc.next();
+            System.out.println(a.charAt(0)+a.substring(a.length()-1 ,a.length()));
+        }
+    }
+}
+
+
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int t = sc.nextInt();
+//        for(int i=0; i<t; i++){
+//            String a = sc.next();
+//            System.out.println(a.charAt(0)+a.substring(a.length()-1 ,a.length()));
+//        }
+//    }
+//}
