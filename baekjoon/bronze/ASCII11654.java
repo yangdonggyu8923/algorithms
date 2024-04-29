35e5cf6da9c86c81dc2ac788779e1ce6d03a2cfa
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-04-29 21:05:16 +0900

    240429

---
+++ baekjoon/bronze/ASCII11654.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class ASCII11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.next().charAt(0);
+        System.out.println(a);
+
+
+    }
+}
