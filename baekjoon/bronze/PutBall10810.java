b890ab097496c8fbd0e1f927155c194c029d0422
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-05-13 18:49:01 +0900

    240513

---
+++ baekjoon/bronze/PutBall10810.java
@@ -0,0 +1,24 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class PutBall10810 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int m = sc.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            for (int j = a - 1; j < b; j++) {
+                arr[j] = c;
+            }
+        }
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k] + " ");
+        }
+    }
+}
