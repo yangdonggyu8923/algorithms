3d03ec57cb56f9b7c86f448f8c575ddf5cba587b
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-05-21 18:14:05 +0900

    240521

---
+++ baekjoon/bronze/N2741.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class N2741 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=0; i<n; i++){
+            System.out.println(i+1);
+        }
+    }
+}
