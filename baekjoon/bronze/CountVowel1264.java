463223576bcec4a6b8152ee0ac7ecdcdd1ba1fd8
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-06-18 20:00:48 +0900

    baek 1264

---
+++ baekjoon/bronze/CountVowel1264.java
@@ -0,0 +1,24 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class CountVowel1264 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String s = sc.nextLine();
+            if (s.equals("#")) {
+                break;
+            }
+            int count = 0;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                        || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
