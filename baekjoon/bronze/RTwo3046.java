20f41996db838f623bd00eba819c95db8216cffb
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-07-03 19:10:17 +0900

    baek 3046

---
+++ baekjoon/bronze/RTwo3046.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class RTwo3046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r1 = sc.nextInt();
+        int s = sc.nextInt();
+
+        System.out.println(2*s-r1);
+    }
+}
